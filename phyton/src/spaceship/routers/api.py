cfef91b2a31b82327f888ba1077ad43399a272c8
Michael Chirozidi <maikl.tigriv@gmail.com>
2023-05-19 18:12:22 +0200

    Upgraded API: added new endpoint /matrix

--- phyton/src/spaceship/routers/api.py
+++ phyton/src/spaceship/routers/api.py
@@ -1,3 +1,4 @@
+import numpy as np
 from fastapi import APIRouter
 
 router = APIRouter()
@@ -6,3 +7,18 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.get('/matrix')
+def matrix() -> dict:
+    matrix_a = np.random.rand(10, 10)
+    matrix_b = np.random.rand(10, 10)
+
+    product = np.dot(matrix_a, matrix_b)
+
+    result = {
+        "matrix_a": matrix_a.tolist(),
+        "matrix_b": matrix_b.tolist(),
+        "product": product.tolist()
+    }
+
+    return result
