f4e743327f48c637ace14f5fe720a8ccf203d2a6
Michael Chirozidi <maikl.tigriv@gmail.com>
2023-05-29 18:57:14 +0200

    Added Dockerfile for Kotlin-module

--- kotlin/src/main/kotlin/michigang1/me/Application.kt
+++ kotlin/src/main/kotlin/michigang1/me/Application.kt
@@ -4,7 +4,7 @@ import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import michigang1.me.plugins.*
-
+// it is main function
 fun main() {
     embeddedServer(Netty, port = 8080, host = "localhost", module = Application::module)
         .start(wait = true)
